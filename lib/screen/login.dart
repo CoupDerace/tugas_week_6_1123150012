5879751d36deee58d4c9439b6b08fbfb961d7b90
CoupDerace <turtusibae@gmail.com>
2025-10-31 14:42:03 +0700

    menambahkan border pada kolom email

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,42 +6,57 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            body: Center(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  children: [
-                    SizedBox(height: 5),
-                    Icon(Icons.qr_code_scanner_sharp, size: 60, color: Colors.purple),
-                    SizedBox(height: 10),
-                    Text(
-                      "Welkam Brok",
-                      style: TextStyle(
-                        fontSize: 30.0, 
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black
-                        ),
-                    ),
-                    SizedBox(height: 20),
-                    Text("Sung ae login",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.grey
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      hintText: "Masukkan Email",
-                      prefixIcon: Icon(Icons.email_outlined),
-                    ),
-                  ),
-                  ],
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 5),
+              const Icon(Icons.qr_code_scanner_sharp, size: 60, color: Colors.purple),
+              const SizedBox(height: 10),
+              const Text(
+                "Welkam Brok",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
-            ),
-          );
+              const SizedBox(height: 20),
+              const Text(
+                "Sung ae login",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "Masukkan Email",
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.purple[300]!),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.blue[700]!,
+                      width: 2,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
